0f25bc42af405c1c77993b07c3fdf21f0945b857
krishanthan4 <krishanthan2022.4.4@gmail.com>
2024-03-24 13:42:17 +0530

    test

--- scripts/seed.js
+++ scripts/seed.js
@@ -16,9 +16,7 @@ async function seedUsers(client) {
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-      );
-    `;
+        password TEXT NOT NULL);`;
 
     console.log(`Created "users" table`);
 
